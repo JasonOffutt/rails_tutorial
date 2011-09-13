24c4460b358ce05b678239c50d1897e5277dab60
JasonOffutt <jason.offutt@gmail.com>
2011-09-12 17:34:16 -0700

    More work on User object and authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,11 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  # Virtual properties (don't exist in db)
+  attr_accessor :password
+  
+  # Accessible properties
+  attr_accessible :name, :email, :password, :password_confirmation
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
   validates :name,  :presence => true,
@@ -19,4 +23,42 @@ class User < ActiveRecord::Base
   validates :email, :presence => true,
                     :format => { :with => email_regex },
                     :uniqueness => { :case_sensitive => false }
+                    
+  validates :password,  :presence => true,
+                        :confirmation => true,
+                        :length => { :within => 6..40 }
+                        
+  before_save :encrypt_password
+  
+  # Return true if the user's password matches the submitted password
+  def has_password?(submitted_password)
+    # Compare encrypted_password with the encrypted version of submitted_password
+    encrypted_password == encrypt(submitted_password)
+  end
+  
+  # Static/Class methods
+  def self.authenticate(email, submitted_password)
+    user = find_by_email email
+    return nil if user.nil?
+    return user if user.has_password? submitted_password
+  end
+  
+  private
+  
+    def encrypt_password
+      self.salt = make_salt if new_record? # Using ActiveRecord goodness to make sure this only gets created once.
+      self.encrypted_password = encrypt(password)
+    end
+    
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
+    
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+    
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
 end
